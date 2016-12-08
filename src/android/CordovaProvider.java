959974877e0682fdf5df4ea6951f685f5538499d
Gerjan Vlot <27927@hoornbeeck.nl>
2016-12-08 11:38:54 +0100

    x

--- src/android/CordovaProvider.java
+++ src/android/CordovaProvider.java
@@ -12,19 +12,25 @@ import java.util.List;
  */
 
 public class CordovaProvider {
+
     private IBarcodeReaderManager _barcodeReaderManager;
     private CordovaInterface _cordovaInterface;
+
+
     public CordovaProvider(CordovaInterface cordovaInterface){
         _cordovaInterface = cordovaInterface;
     }
     public void start(final org.apache.cordova.CallbackContext callbackContext){
         try {
             _barcodeReaderManager = new BarcodeReaderManager(_cordovaInterface.getActivity());
+
             _barcodeReaderManager.setOnReadyCallback(new CallbackContext() {
                 @Override
                 public void onReady() {
+                    Log.d(getClass().getSimpleName(), "Sensor Ready");
                     _barcodeReaderManager.setScannerDevice(1);
                     try {
+                        Log.d(getClass().getSimpleName(), "Starting Scan...");
                         _barcodeReaderManager.start();
                     } catch (Exception ex) {
                         Log.e(getClass().getSimpleName(), ex.toString());
@@ -34,9 +40,11 @@ public class CordovaProvider {
                 @Override
                 public void onScanResult(String data) {
                     try {
+                        Log.d(getClass().getSimpleName(), "ScanResult: " + data);
                         PluginResult result = new PluginResult(PluginResult.Status.OK, data);
                         result.setKeepCallback(true);
                         callbackContext.sendPluginResult(result);
+                        Log.d(getClass().getSimpleName(), "Stopping scan...: " + data);
                         _barcodeReaderManager.stop();
                         _barcodeReaderManager.close();
                     }catch (Exception ex){
