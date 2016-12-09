c2f5fc793b4fee179cca54839a20c652fe854cbe
Jan Overduin <servicedesk@aspect-ict.nl>
2016-12-09 14:03:50 +0100

    Update CordovaProvider.java

--- src/android/CordovaProvider.java
+++ src/android/CordovaProvider.java
@@ -20,17 +20,16 @@ public class CordovaProvider {
     public CordovaProvider(CordovaInterface cordovaInterface){
         _cordovaInterface = cordovaInterface;
     }
-    public void stop(final org.apache.cordova.CallbackContext callbackContext){
-         try {
-                        
-                        Log.d(getClass().getSimpleName(), "Stopping scan...: " + data);
-                        _barcodeReaderManager.stop();
-                        _barcodeReaderManager.close();
-                        callbackContext.success();
-                    }catch (Exception ex){
-                        ex.printStackTrace();
-                        callbackContext.error(ex.getMessage());
-                    }
+    public void stop(final org.apache.cordova.CallbackContext callbackContext) {
+        try {
+            Log.d(getClass().getSimpleName(), "Stopping scan...: ");
+            _barcodeReaderManager.stop();
+            _barcodeReaderManager.close();
+            callbackContext.success();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            callbackContext.error(ex.getMessage());
+        }
     }
     public void start(final org.apache.cordova.CallbackContext callbackContext){
         try {
