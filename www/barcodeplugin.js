be7ba74cf96d4eddf01a222f65c7312e1fe7aa9f
Jan Overduin <servicedesk@aspect-ict.nl>
2016-12-15 14:45:21 +0100

    Update barcodeplugin.js

--- www/barcodeplugin.js
+++ www/barcodeplugin.js
@@ -1,56 +1,63 @@
 /*global cordova, module*/
-var Promise = (function () {
+var Promise = (function() {
     function Promise() {
-        this.onSuccesListener = function () { };
-        this.onErrorListener = function () { };
-        this.onFinalizeListener = function () { };
-        this.onRejectListener = function () { };
-        this.onNotifyListener = function () { };
+        this.onSuccesListener = function() {};
+        this.onErrorListener = function() {};
+        this.onFinalizeListener = function() {};
+        this.onRejectListener = function() {};
+        this.onNotifyListener = function() {};
     }
-    Promise.prototype.then = function (successCallback, errorCallback, notifyCallback) {
+    Promise.prototype.then = function(successCallback, errorCallback, notifyCallback) {
         if (successCallback != undefined) {
-            this.onSuccesListener = function (promiseValue) { return successCallback(promiseValue); };
+            this.onSuccesListener = function(promiseValue) {
+                return successCallback(promiseValue);
+            };
         }
         if (errorCallback != undefined) {
-            this.onErrorListener = function (reason) { return errorCallback(reason); };
+            this.onErrorListener = function(reason) {
+                return errorCallback(reason);
+            };
         }
         if (notifyCallback != undefined) {
-            this.onNotifyListener = function (state) { return errorCallback(state); };
+            this.onNotifyListener = function(state) {
+                return errorCallback(state);
+            };
         }
         return this;
     };
-    Promise.prototype.catch = function (onRejected) {
+    Promise.prototype.catch = function(onRejected) {
         if (onRejected != undefined) {
-            this.onRejectListener = function (reason) { return onRejected(reason); };
+            this.onRejectListener = function(reason) {
+                return onRejected(reason);
+            };
         }
         return this;
     };
-    Promise.prototype.finally = function (finallyCallback) {
-        this.onFinalizeListener = function () { return finallyCallback(); };
+    Promise.prototype.finally = function(finallyCallback) {
+        this.onFinalizeListener = function() {
+            return finallyCallback();
+        };
     };
-    Promise.prototype.resolve = function (value) {
+    Promise.prototype.resolve = function(value) {
         if (value != undefined) {
             this.onSuccesListener(value);
             this.onFinalizeListener();
-        }
-        else {
+        } else {
             this.onSuccesListener();
         }
     };
-    Promise.prototype.reject = function (reason) {
+    Promise.prototype.reject = function(reason) {
         if (reason != undefined) {
             this.onRejectListener(reason);
             this.onFinalizeListener();
-        }
-        else {
+        } else {
             this.onRejectListener();
         }
     };
-    Promise.prototype.notify = function (state) {
+    Promise.prototype.notify = function(state) {
         if (state != undefined) {
             this.onNotifyListener(state);
-        }
-        else {
+        } else {
             this.onNotifyListener();
         }
     };
@@ -58,40 +65,40 @@ var Promise = (function () {
 })();
 // js interface to plugin
 module.exports = {
-	start: function () {
-		var promise = new Promise();
-        cordova.exec(function(){
-			promise.resolve();
-		}, function(ex){
-			promise.reject(ex);
-		}, "BarcodePlugin", "start", []);
-		return promise;
+    start: function() {
+        var promise = new Promise();
+        cordova.exec(function() {
+            promise.resolve();
+        }, function(ex) {
+            promise.reject(ex);
+        }, "BarcodePlugin", "start", []);
+        return promise;
     },
-	stop: function () {
-		var promise = new Promise();
-        cordova.exec(function(){
-			promise.resolve();
-		}, function(ex){
-			promise.reject(ex);
-		}, "BarcodePlugin", "stop", []);
-		return promise;
+    stop: function() {
+        var promise = new Promise();
+        cordova.exec(function() {
+            promise.resolve();
+        }, function(ex) {
+            promise.reject(ex);
+        }, "BarcodePlugin", "stop", []);
+        return promise;
     },
-	initialize: function () {
-		var promise = new Promise();
-        cordova.exec(function(){
-			promise.resolve();
-		}, function(ex){
-			promise.reject(ex);
-		}, "BarcodePlugin", "initialize", []);
-		return promise;
+    initialize: function() {
+        var promise = new Promise();
+        cordova.exec(function() {
+            promise.resolve();
+        }, function(ex) {
+            promise.reject(ex);
+        }, "BarcodePlugin", "initialize", []);
+        return promise;
     },
-	onScanResult: function () {
-		var promise = new Promise();
-        cordova.exec(function(res){
-			promise.resolve(res);
-		}, function(ex){
-			promise.reject(ex);
-		}, "BarcodePlugin", "onScanResult", []);
-		return promise;
-    } 
+    onScanResult: function() {
+        var promise = new Promise();
+        cordova.exec(function(res) {
+            promise.resolve(res);
+        }, function(ex) {
+            promise.reject(ex);
+        }, "BarcodePlugin", "onScanResult", []);
+        return promise;
+    }
 };
